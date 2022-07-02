e1d15993236ba6a2f875e0c92f3cea38cbd8b5e3
Shaharina Mita <S.mita90232@gmail.com>
2022-07-02 21:59:38 +0600

    added function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import {} from 'module';
+import Countries from '../public/Components/Countries/Countries';
 
 function App() {
     return (
