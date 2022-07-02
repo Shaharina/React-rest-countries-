59d35e25ecf051e699e536543914f4a5ca045057
Shaharina Mita <S.mita90232@gmail.com>
2022-07-02 22:36:48 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import Countries from '../public/Components/Countries/Countries';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
-            <Countries></Countries>
+            <h1>Hello World</h1>
+            <Countri 
         </div>
     );
 }
-// function LoadCountries() {
-//     const [countries, setCountries] = useState([]);
-//     useEffect(() => {
-//         fetch('https://restcountries.com/v3.1/all')
-//             .then(res => res.json())
-//             .then(data => setCountries(data));
-//     }, []);
-//     return (
-//         <div>
-//             <h1>Visiting Every Country Of The World </h1>
-//             <h3>Available Countries: {countries.length} </h3>
-
-//             {countries.map(country => (
-//                 <Country name={country.name.common}></Country>
-//             ))}
-//         </div>
-//     );
-// }
-
-// function Country(props) {
-//     return (
-//         <div>
-//             <h2>Name:{props.name} </h2>
-//             <h4>Population:{props.population}</h4>
-//         </div>
-//     );
-// }
 
 export default App;
