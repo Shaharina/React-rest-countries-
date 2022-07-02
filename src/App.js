31e5071bb22ac0c35f794d80898c5a46ebd3865b
Shaharina Mita <S.mita90232@gmail.com>
2022-07-02 22:49:54 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Countries from '../public/Components/Countries/Countries';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
+            <Countries></Countries>
             <h1>Hello World</h1>
-            <Countri 
         </div>
     );
 }
