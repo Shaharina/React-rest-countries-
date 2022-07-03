be4ae877cf1c8cd5d4e03a7312f16d5b6c2c6df6
Shaharina Mita <S.mita90232@gmail.com>
2022-07-03 17:20:00 +0600

    style added

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Country.css';
 
 const Country = props => {
-    console.log(props.country);
-    const { area, region, population, name } = props.country;
+    // console.log(props.country);
+    const { area, region, population, name, flags } = props.country;
 
     return (
         <div className='country'>
             <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt='' />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
 
